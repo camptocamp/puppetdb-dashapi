a9283954b789e21750862174e3ff0a46f89e91bb
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2016-03-01 17:01:57 +0100

    Default to no SSL

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 PUPPETDB_HOST = 'puppetdb'
 PUPPETDB_PORT = '8080'
 PUPPETDB_SSL_VERIFY = False
-PUPPETDB_KEY = '/app/dashapi.key'
-PUPPETDB_CERT = '/app/dashapi.crt'
+PUPPETDB_KEY = None
+PUPPETDB_CERT = None
